7245bb25fe302cb27f040f00fa7ca3771b39369c
robot9001 <robo9k@symlink.io>
2022-02-28 21:50:04 +0100

    CLI MVP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let comic_json: serde_json::Value = reqwest::Client::new()
+        .get("https://xkcd.com/info.0.json")
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", comic_json);
+    Ok(())
 }
