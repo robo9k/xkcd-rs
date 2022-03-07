a2052e66ed755b2678b7eb3015b122351578f4cb
robot9001 <robo9k@symlink.io>
2022-03-07 23:29:17 +0100

    Add TODOs for `tower-http`

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,18 @@ async fn main() -> Result<(), reqwest::Error> {
         .json()
         .await?;
 
+    // Abstract TODOs for `tower-http`
+    // - construct hyper client with TLS as default impl
+    // - create tower client service fn (with decompression layer ?) from hyper client
+    // - set `user-agent` HTTP request header
+    // - set `accept` HTTP request header (in "comic by id" request only ?) ?
+    // - call HTTP client service fn
+    // - check HTTP response status ?
+    // - check response `content-length` ?
+    // - check response `content-type` ?
+    // - convert response to BytesMut buffer or such
+    // - convert buffer to `xckd:Comic` via serde
+
     println!("{:#?}", comic_json);
     Ok(())
 }
